7e5d6f283dd48540743cc6a8bead5e98828f6d29
miracelwhipp <miracelwhipp@gmx.de>
2018-09-29 13:35:13 +0200

    added some analysis

--- deployment/before-deploy.sh
+++ deployment/before-deploy.sh
@@ -5,4 +5,14 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
     openssl aes-256-cbc -K $encrypted_5260ec8c84f7_key -iv $encrypted_5260ec8c84f7_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
 
     gpg2 --fast-import --allow-non-selfsigned-uid deployment/codesigning.asc
+
+    echo "decrypted file"
+    head -n3 deployment/codesigning.asc
+
+    echo "keys"
+    gpg2 --list-keys
+
+    echo "secret keys"
+    gpg2 --list-secret-keys
+
 fi
