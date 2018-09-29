572c568c9872bf28956f2af691ece1375b6a06e5
miracelwhipp <miracelwhipp@gmx.de>
2018-09-29 13:47:06 +0200

    again: encrypt travis settings and files with linux shell - never with windows cmd.exe!

--- deployment/before-deploy.sh
+++ deployment/before-deploy.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 
-    openssl aes-256-cbc -K $encrypted_5260ec8c84f7_key -iv $encrypted_5260ec8c84f7_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_35d725154913_key -iv $encrypted_35d725154913_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
 
     gpg2 --fast-import --allow-non-selfsigned-uid deployment/codesigning.asc
 
