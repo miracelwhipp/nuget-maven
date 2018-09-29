9d2b1c92333524aea565b1df6d311900f7ca586a
miracelwhipp <miracelwhipp@gmx.de>
2018-09-29 14:38:48 +0200

    corrected encrypted passwords

--- deployment/before-deploy.sh
+++ deployment/before-deploy.sh
@@ -5,14 +5,4 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
     openssl aes-256-cbc -K $encrypted_35d725154913_key -iv $encrypted_35d725154913_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
 
     gpg2 --fast-import --allow-non-selfsigned-uid deployment/codesigning.asc
-
-    echo "decrypted file"
-    head -n3 deployment/codesigning.asc
-
-    echo "keys"
-    gpg2 --list-keys
-
-    echo "secret keys"
-    gpg2 --list-secret-keys
-
 fi
